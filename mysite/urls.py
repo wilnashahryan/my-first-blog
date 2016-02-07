c8abb83e44e503fdf538d4966804a48220b28928
wilnashahryan <willnashahryan@gmail.com>
2016-02-08 02:11:56 +0800

    Changed the HTML for the site

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,22 +1,16 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Import the include() function: from django.conf.urls import url, include
-    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'',include('blog.urls')),
 ]
+
+#eg: http://www.mysite.com/post/12345/
+#^post/(\d+)/$
+#(\d+) means that there will be a number (one or more digits) and that we want the number captured and extracted
+#^ for beginning of the text
+#$ for end of text
+#\d for a digit
+#+ to indicate that the previous item should be repeated at least once
+#() to capture part of the pattern
